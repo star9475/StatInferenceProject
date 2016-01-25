7a8fab02826c56e32bb3ec53f184c241e4e05d36
star9475 <star9475@hotmail.com>
2016-01-24 22:19:04 -0800

    Graphs and init analysis done.  Need to summarize conlclusions.

--- StatInfProject.R
+++ StatInfProject.R
@@ -7,6 +7,13 @@ set.seed(1623); qplot(rexp(1000,2), geom="histogram")
 set.seed(1623); mns = NULL; 
 for (i in 1 : 1000) mns = c(mns, mean(rexp(40,.2))); 
 
+
+set.seed(1623);
+meandata <- data.frame(
+     x = c(apply(matrix(rexp(nosim * nosample, .2), nosim), 1, mean)),
+     n = factor(rep(c(nosample), c(nosample))) 
+     )
+
 p <- qplot(mns,
            geom="histogram",
            main = "Average Means of 40 Samples",
@@ -16,3 +23,20 @@ p <- qplot(mns,
            alpha=I(.2)
 )
 p + geom_vline(xintercept=5)
+
+set.seed(1623); vars = NULL; 
+for (i in 1 : 1000) mns = c(vars, var(rexp(40,.2))); 
+
+
+
+g <- ggplot(data = meandata, aes(x = x) ,xlab = "Means") +
+     geom_histogram(aes(y = ..density..), fill = ("blue"), col=("darkgreen"), alpha=.4) +
+     labs(title ="Average Means of 40 Samples", x="means") +
+     geom_vline(xintercept=5)
+g
+
+
+
+p <- ggplot(data = ToothGrowth, aes(x=dose,y=len )) + 
+     geom_point() +facet_wrap(~supp) + geom_smooth() 
+     
